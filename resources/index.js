9c1096072cea155285cd046df18f32c15fe92940
arthursr01 <70337560+arthursr01@users.noreply.github.com>
2020-11-08 20:46:36 -0500

    Update index.js

--- resources/index.js
+++ resources/index.js
@@ -9,4 +9,35 @@ function myFunction() {
     startBtn.style.display = 'none';
     
   }
-  
+  function controlPlayer1(){
+    switch(e.keyCode) {
+      case 65:
+          deltaA -= 2;
+          break;
+      case 87:
+          deltaB -= 2;
+          break;
+      case 68:
+          deltaA += 2;
+          break;
+      case 83:
+          deltaB += 2;
+          break;
+  }
+  }
+  function controlPlayer2() {
+    switch(e.keyCode) {
+      case 37:
+          deltaX -= 2;
+          break;
+      case 38:
+          deltaY -= 2;
+          break;
+      case 39:
+          deltaX += 2;
+          break;
+      case 40:
+          deltaY += 2;
+          break;
+  }
+}
