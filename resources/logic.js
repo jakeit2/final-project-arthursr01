9a09939b0425948ac7692b070a1bcc291eecaa3d
jakeit2 <donawesome8@gmail.com>
2020-11-18 15:13:59 -0500

    commit

--- resources/logic.js
+++ resources/logic.js
@@ -4,6 +4,7 @@ const selectScreen = document.getElementById("selectBike");
 
 // instantiate the game object
 let game;
+let outcome, winner, playerCount = Player.allInstances.length;
 
 // hide gamescreen
 selectScreen.classList.add(`d-none`);
@@ -13,3 +14,14 @@ startGameButton.addEventListener(`click`, function(startGameButtonEvent){
      welcomeScreen.classList.add("d-none");
      selectScreen.classList.remove("d-none");
 })
+
+function draw(){
+     if (playerCount.allInstances.filter(p => !p.key).length == 0){
+          if(playerCount == 1) {
+               const alivePlayers = Player.allInstances.filter(p => p.dead == false);
+               outcome = `Player ${alivePlayers[0].playerId} wins!`;
+          } else if (playerCount == 0) {
+               outcome = 'Draw!';
+          }
+     }
+}
