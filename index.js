0f5ab80168a21a05ab8166b889f44f18947efcf7
jakeit2 <donawesome8@gmail.com>
2020-12-03 16:42:29 -0500

    commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const endgameResults = document.getElementById("gameHistoryResults");
 var canvas = document.getElementById("tronCanvas");
 const unit = 15;
 const context = canvas.getContext("2d");
-var objImage= null;
+var objImage = null;
 var playerSpeed = 200;
 // hide gamescreen
 //selectScreen.classList.add(`d-none`);
@@ -38,33 +38,33 @@ let p2 = new Player(unit * 43, unit * 43, "#FF5050");
 
 
 function openMenu() {
-  var pickBike = document.getElementById("Menu");
-  if (pickBike.style.display === "none") {
-    pickBike.style.display = "none";
+  //var pickBike = document.getElementById("Menu");
+  //if (pickBike.style.display === "none") {
+    //pickBike.style.display = "none";
     startBtn.style.visibility = "hidden";
     window.addEventListener("DOMContentLoaded", event => {
       const audio = document.querySelector("audio");
       audio.volume = 0.2;
       audio.play();
     });
-  } else {
-    pickBike.style.display = "none"
-  }
+  //} else {
+    //pickBike.style.display = "none"
+  //}
+  //drawSprites("resources/Images/yellow-bike.png", 10, 10);
+  getPlayableCells(canvas);
+  drawBackground();
+  drawStartingPositions(Player.allInstances);
+  setInterval(draw, 100);
 
 }
-  function startGame(){
-      var blueBike = document.getElementById("blue");
+function startGame() {
+  var blueBike = document.getElementById("blue");
 
-  }
-function myFunction() {
-  
-    //drawSprites("resources/Images/yellow-bike.png", 10, 10);
-    getPlayableCells(canvas);
-    drawBackground();
-    drawStartingPositions(Player.allInstances);
-    setInterval(draw,100);
+}
 
-  }
+
+
+
 /*
 function drawSprites(url, pos1, pos2){
   var p = new Image();
@@ -74,88 +74,88 @@ function drawSprites(url, pos1, pos2){
   });
 }
 */
-  function getPlayableCells(canvas, unit) {
-    let playableCells = new Set();
-    for (let i = 0; i < canvas.width / unit; i++){
-         for (let j = 0; j < canvas.height / unit; j++){
-              playableCells.add(`${i * unit}x${j * unit}y`);
-         };
+function getPlayableCells(canvas, unit) {
+  let playableCells = new Set();
+  for (let i = 0; i < canvas.width / unit; i++) {
+    for (let j = 0; j < canvas.height / unit; j++) {
+      playableCells.add(`${i * unit}x${j * unit}y`);
     };
-    return playableCells;
+  };
+  return playableCells;
 };
 
 let playableCells = getPlayableCells(canvas, unit);
 
-  function drawBackground() {
-    context.strokeStyle = '#001900';
-    for (let i = 0; i <= canvas.width / unit + 2; i += 2) {
-      for (let j = 0; j <= canvas.height / unit + 2; j += 2) {
-        context.strokeRect(0, 0, unit * i, unit * j);
-      };
+function drawBackground() {
+  context.strokeStyle = '#001900';
+  for (let i = 0; i <= canvas.width / unit + 2; i += 2) {
+    for (let j = 0; j <= canvas.height / unit + 2; j += 2) {
+      context.strokeRect(0, 0, unit * i, unit * j);
     };
-    context.strokeStyle = '#000000';
-    context.lineWidth = 2;
-    for (let i = 1; i <= canvas.width / unit; i += 2) {
-      for (let j = 1; j <= canvas.height / unit; j += 2) {
-        context.strokeRect(0, 0, unit * i, unit * j);
-      };
-    };
-    context.lineWidth = 1;
   };
-  drawBackground();
+  context.strokeStyle = '#000000';
+  context.lineWidth = 2;
+  for (let i = 1; i <= canvas.width / unit; i += 2) {
+    for (let j = 1; j <= canvas.height / unit; j += 2) {
+      context.strokeRect(0, 0, unit * i, unit * j);
+    };
+  };
+  context.lineWidth = 1;
+};
+drawBackground();
 
-  function drawStartingPositions(players) {
-    players.forEach(p => {
-      context.fillStyle = p.color;
-      context.fillRect(p.x, p.y, unit, unit);
-      context.strokeStyle = 'black';
-      context.strokeRect(p.x, p.y, unit, unit);
-    });
-  };
-  
-  
-  
-  function setKey(key, player, up, right, down, left) {
-    switch (key) {
-      case up:
-        if (player.direction !== "DOWN") {
-          player.key = "UP";
-        }
-        break;
-      case right:
-        if (player.direction !== "LEFT") {
-          player.key = "RIGHT";
-        }
-        break;
-      case down:
-        if (player.direction !== "UP") {
-          player.key = "DOWN";
-        }
-        break;
-      case left:
-        if (player.direction !== "RIGHT") {
-          player.key = "LEFT";
-        }
-        break;
-      default:
-        break;
-    }
+function drawStartingPositions(players) {
+  players.forEach(p => {
+    context.fillStyle = p.color;
+    context.fillRect(p.x, p.y, unit, unit);
+    context.strokeStyle = 'black';
+    context.strokeRect(p.x, p.y, unit, unit);
+  });
+};
+
+
+
+function setKey(key, player, up, right, down, left) {
+  switch (key) {
+    case up:
+      if (player.direction !== "DOWN") {
+        player.key = "UP";
+      }
+      break;
+    case right:
+      if (player.direction !== "LEFT") {
+        player.key = "RIGHT";
+      }
+      break;
+    case down:
+      if (player.direction !== "UP") {
+        player.key = "DOWN";
+      }
+      break;
+    case left:
+      if (player.direction !== "RIGHT") {
+        player.key = "LEFT";
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+function handleKeyPress(event) {
+  let key = event.keyCode;
+
+  if (key === 37 || key === 38 || key === 39 || key === 40) {
+    event.preventDefault();
   }
 
-  function handleKeyPress(event) {
-    let key = event.keyCode;
-  
-    if (key === 37 || key === 38 || key === 39 || key === 40) {
-      event.preventDefault();
-    }
-  
-    setKey(key, p1, 38, 39, 40, 37);
-    setKey(key, p2, 87, 68, 83, 65); 
-  }
+  setKey(key, p1, 38, 39, 40, 37);
+  setKey(key, p2, 87, 68, 83, 65);
+}
 
 document.addEventListener('keydown', handleKeyPress);
 
-function draw(){
+function draw() {
   Player.allInstances.forEach((p) => {
     if (p.key) {
       p.direction = p.key;
@@ -186,23 +186,23 @@ function draw(){
 
 let outcome, winner, playerCount = Player.allInstances.length;
 
-function determineWinner(){
-  if (playerCount.allInstances.filter(p => !p.key).length == 0){
-       if(playerCount == 1) {
-            const alivePlayers = Player.allInstances.filter(p => p.dead == false);
-            outcome = `Player ${alivePlayers[0].playerId} wins!`;
-       } else if (playerCount == 0) {
-            outcome = 'Draw!';
-       }
+function determineWinner() {
+  if (playerCount.allInstances.filter(p => !p.key).length == 0) {
+    if (playerCount == 1) {
+      const alivePlayers = Player.allInstances.filter(p => p.dead == false);
+      outcome = `Player ${alivePlayers[0].playerId} wins!`;
+    } else if (playerCount == 0) {
+      outcome = 'Draw!';
+    }
 
-       if (outcome) {
-        function myPlay(){
-          var audio = new Audio("resources/music/Bike-Crash.mp3");
-          audio.play();
+    if (outcome) {
+      function myPlay() {
+        var audio = new Audio("resources/music/Bike-Crash.mp3");
+        audio.play();
       }
-            createEndScreen(winner);
-            clearInterval(game);
-       }
+      createEndScreen(winner);
+      clearInterval(game);
+    }
   }
 
 }
@@ -244,13 +244,13 @@ function createEndScreen(color) {
 
 }
 
-function resetGame(){
+function resetGame() {
   outcome = '';
   winner = '';
 
   context.clearRect(0, 0, canvas.width, canvas.height);
   clearInterval(game);
-  game = setInterval(draw,100);
+  game = setInterval(draw, 100);
 }
 
 
